5d0b63f52a7278a3781d72990365abfea11bff79
Bollineni Narasimha Naidu <146934168+narasimha779922@users.noreply.github.com>
2025-11-25 11:53:08 +0530

    Add files via upload

---
+++ loan data.sql
@@ -0,0 +1,114 @@
+use finance;
+select * from bank_loan_data;
+SELECT COUNT(id) AS Total_Applications FROM bank_loan_data;
+SELECT COUNT(id) AS Total_Applications FROM bank_loan_data
+WHERE MONTH(issue_date) = 12 ;
+
+SELECT COUNT(id) AS Total_Applications FROM bank_loan_data WHERE MONTH(issue_date) = 11;
+
+SELECT AVG(dti)*100 AS PMTD_Avg_DTI FROM bank_loan_data
+WHERE MONTH(issue_date) = 11;
+
+SELECT SUM(loan_amount) AS Total_Funded_Amount FROM bank_loan_data;
+
+SELECT SUM(loan_amount) AS Total_Funded_Amount FROM bank_loan_data
+WHERE MONTH(issue_date) = 12;
+
+SELECT SUM(loan_amount) AS Total_Funded_Amount FROM bank_loan_data
+WHERE MONTH(issue_date) = 11;
+
+SELECT SUM(total_payment) AS Total_Amount_Collected FROM bank_loan_data
+WHERE MONTH(issue_date) = 12;
+SELECT AVG(int_rate)*100 AS PMTD_Avg_Int_Rate FROM bank_loan_data
+WHERE MONTH(issue_date);
+
+SELECT 
+	home_ownership AS Home_Ownership, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY home_ownership
+ORDER BY home_ownership;
+
+SELECT 
+	purpose AS PURPOSE, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY purpose
+ORDER BY purpose;
+
+SELECT 
+	emp_length AS Employee_Length, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY emp_length
+ORDER BY emp_length;
+
+SELECT 
+	term AS Term, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY term
+ORDER BY term;
+
+SELECT 
+	address_state AS State, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY address_state
+ORDER BY address_state;
+
+
+SELECT 
+	MONTH(issue_date) AS Month_Munber, 
+	DATENAME(MONTH, issue_date) AS Month_name, 
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY MONTH(issue_date), DATENAME(MONTH, issue_date)
+ORDER BY MONTH(issue_date);
+
+SELECT 
+	loan_status, 
+	SUM(total_payment) AS MTD_Total_Amount_Received, 
+	SUM(loan_amount) AS MTD_Total_Funded_Amount 
+FROM bank_loan_data
+WHERE MONTH(issue_date) = 12 
+GROUP BY loan_status;
+
+SELECT
+        loan_status,
+        COUNT(id) AS LoanCount,
+        SUM(total_payment) AS Total_Amount_Received,
+        SUM(loan_amount) AS Total_Funded_Amount,
+        AVG(int_rate * 100) AS Interest_Rate,
+        AVG(dti * 100) AS DTI
+    FROM
+        bank_loan_data
+    GROUP BY
+        loan_status;
+        
+        SELECT SUM(total_payment) AS Bad_Loan_amount_received FROM bank_loan_data
+WHERE loan_status = 'Charged Off';
+
+SELECT SUM(loan_amount) AS Bad_Loan_Funded_amount FROM bank_loan_data
+WHERE loan_status = 'Charged Off';
+
+SELECT COUNT(id) AS Bad_Loan_Applications FROM bank_loan_data
+WHERE loan_status = 'Charged Off';
+
+SELECT
+    (COUNT(CASE WHEN loan_status = 'Charged Off' THEN id END) * 100.0) / COUNT(id) AS Bad_Loan_Percentage
+FROM bank_loan_data; 
+
+the pasid to each employee to _read finace to each sector to ramndaom to each to working to lang _month to each of the gain to learn  to maintain each other to wotking ;
